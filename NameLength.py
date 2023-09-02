391fb2baba8e2b3c6ab49caa711472842b97c015
Youssef <youssefelbaye@gmail.com>
2023-09-02 23:25:21 +0200

    Create NameLength.py

---
+++ NameLength.py
@@ -0,0 +1,8 @@
+# General welcome
+print("Welcome to our application")
+# Ask users name and save it in variable
+user_name = input("What is your name?\n")
+# print welcome letter
+print("Nice to meet you," + user_name)
+# show message of user name letter number
+print(len(user_name))
